b7c1ca401bdd0af44e7ef3d8aa6b7d351a107064
Benan <257623221+Benan13@users.noreply.github.com>
2026-08-12 19:46:33 +0200

    Add files via upload

---
+++ pipeline.py
@@ -0,0 +1,30 @@
+from vector_storage import VectorStorage
+from document_processor import DocumentProcessor
+from llm import Ask_Llm
+class Pipeline:
+    def __init__(self):
+        self.vektor = VectorStorage()
+        self.processor = DocumentProcessor()
+        self.llm = Ask_Llm()
+    def ingestion(self):
+        print("Starting Pipeline")
+        text = self.processor.extract()
+        print("Make Chunks...")
+        chunks = self.processor.make_chunks(text)
+        if len(chunks) > 0:
+            print(f"Chunks: {len(chunks)}")
+            print("Generate Chunks...")
+            self.vektor.add_chunks(chunks)
+            print("I am Finish.")
+        else:
+            print("ERROR: There are no chunks.")
+    def ask_database(self, question: str):
+        relevant_chunks = self.vektor.search(question, n_results=3)
+        context = "\n---\n".join(relevant_chunks)
+        result = self.llm.ask(question=question, type="chat", context=context)
+        return result
+
+
+if __name__  == "__main__":
+    pipeline = Pipeline()
+    pipeline.ingestion()
---
+++ vector_storage.py
@@ -0,0 +1,43 @@
+from sentence_transformers import SentenceTransformer
+import chromadb
+import os
+from dotenv import load_dotenv
+class VectorStorage:
+    def __init__(self, db_name: str="vektor_db"):
+        load_dotenv()
+        HU_KEY = os.getenv("HUGGING_FACE_KEY")
+        self.db_name = db_name
+        self.folder_path = os.path.dirname(os.path.abspath(__file__))
+        self.db_full_path = os.path.join(self.folder_path, self.db_name)
+        self.chroma_client = chromadb.PersistentClient(path=self.db_full_path)
+        try:
+            self.chroma_client._delete(name="pdf_dokumente")
+        except:
+            pass
+        self.collection = self.chroma_client.get_or_create_collection(name="pdf_dokumente")
+        self.model = SentenceTransformer(
+            "sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2",
+            token="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"
+        )
+    def add_chunks(self, chunks: list[str]):
+        BATCH_SIZE = 5461
+        embeddings = self.model.encode(chunks)
+        ids = [f"id_{i}" for i in range(len(chunks))]
+        embedding_list = embeddings.tolist()
+        for i in range(0, len(chunks), BATCH_SIZE):
+            self.collection.add(
+                embeddings=embedding_list[i : i + BATCH_SIZE],
+                documents=chunks[i : i + BATCH_SIZE],
+                ids=ids[i : i + BATCH_SIZE]
+        )
+        print("Daten erfolgreich eingetragen.")
+    def search(self, query_text: str, n_results)-> list[str]:
+        results = self.collection.query(
+            query_texts=[query_text],
+            n_results=n_results
+        )
+        documents = results.get("documents", [[]])
+        if documents:
+            return documents[0]
+if __name__ == "main":
+    vector = VectorStorage()
