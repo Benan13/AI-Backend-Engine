4666f9f07bdde83205edbcc45453af5168d6d7d1
Benan <257623221+Benan13@users.noreply.github.com>
2026-08-12 19:53:10 +0200

    Add files via upload

---
+++ llm.py
@@ -0,0 +1,22 @@
+import ollama
+class Ask_Llm:
+    def ask(self, question: str, type: str, context: str):
+        try:
+            system_prompt = f'Du bist nett und höflich und atwortest du mithilfe des kontexts: {context}'
+            if type == "chat":
+                system_prompt = f"You are polite and answer the questions. You answer with the context: {context}"
+            elif type == "summary":
+                system_prompt=f"Summarize the text in bullet points and if it is useful, use the context: {context}."
+            elif type=="translate":
+                system_prompt = f"Translate the desired text into the desired language and use the context: {context}."
+            stream = ollama.chat(model='qwen2.5:0.5b', messages=[
+                {'role': 'system', 'content': system_prompt},
+                {'role': 'user','content': question},
+                ],
+                stream=True
+                )
+            
+            for chunk in stream:
+                yield chunk['message']['content']
+        except Exception as e:
+            yield f"ERROR: {e}"
