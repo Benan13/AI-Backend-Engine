ca2816e97ad1a988d2a154d54d1e9d7399b33c91
Benan <257623221+Benan13@users.noreply.github.com>
2026-08-14 22:00:20 +0200

    Update document_processor.py

--- document_processor.py
+++ document_processor.py
@@ -1,6 +1,7 @@
 from langchain_community.document_loaders import PyMuPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 import os
+from serverlog import error
 class DocumentProcessor:
     def __init__(self, chunk_size: int=1200, chunk_overlap: int=270):
         self.folder_path = os.path.dirname(os.path.abspath(__file__))
@@ -13,22 +14,24 @@ class DocumentProcessor:
     def extract(self) -> str:
         folder = os.path.join(self.folder_path, "documents")
         documents_list = ""
-        if os.path.exists(folder):
-            for file in os.listdir(folder):
-                if file.endswith(".pdf"):
-                    print(f"loading file: {file}")
-                    loader = PyMuPDFLoader(os.path.join(folder, file))
-                    for doc in loader.load():
-                        documents_list += doc.page_content + "\n"
-        else:
-            print(f"Konnte Ordner nicht finden, lade ihn in {folder}")
-        return documents_list
+        try:
+            if os.path.exists(folder):
+                print("TEST")
+                for file in os.listdir(folder):
+                    if file.endswith(".pdf"):
+                        print(f"loading file: {file}")
+                        loader = PyMuPDFLoader(os.path.join(folder, file))
+                        for doc in loader.load():
+                            documents_list += doc.page_content + "\n"
+                        print(True, file)
+            else:
+                print(f"Konnte Ordner nicht finden, lade ihn in {folder}")
+            return documents_list
+        except Exception as e:
+            error(e)
     def make_chunks(self, text: str) -> list[str]:
-        return self.text_splitters.split_text(text)
+            return self.text_splitters.split_text(text)
 
 
 if __name__ == "__main__":
     processor = DocumentProcessor()
-    text = processor.extract()
-    chunks = processor.make_chunks(text)
-    print(f"Erfolgreich {len(chunks)} Chunks extrahiert!")
