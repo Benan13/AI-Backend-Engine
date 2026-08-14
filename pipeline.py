d8c514e36e89c4b0d4f37614d122377a0518cf6b
Benan <257623221+Benan13@users.noreply.github.com>
2026-08-14 22:01:37 +0200

    Update pipeline.py

--- pipeline.py
+++ pipeline.py
@@ -1,24 +1,31 @@
 from vector_storage import VectorStorage
+from serverlog import error
 from document_processor import DocumentProcessor
+from decider import Decider
 from llm import Ask_Llm
 class Pipeline:
     def __init__(self):
+        self.decider = Decider()
         self.vektor = VectorStorage()
         self.processor = DocumentProcessor()
         self.llm = Ask_Llm()
     def ingestion(self):
-        print("Starting Pipeline")
-        text = self.processor.extract()
-        print("Make Chunks...")
-        chunks = self.processor.make_chunks(text)
-        if len(chunks) > 0:
-            print(f"Chunks: {len(chunks)}")
-            print("Generate Chunks...")
-            self.vektor.add_chunks(chunks)
-            print("I am Finish.")
-        else:
-            print("ERROR: There are no chunks.")
+        try:
+            print("Starting Pipeline")
+            text = self.processor.extract()
+            print("Make Chunks...")
+            chunks = self.processor.make_chunks(text)
+            if len(chunks) > 0:
+                print(f"Chunks: {len(chunks)}")
+                print("Generate Chunks...")
+                self.vektor.add_chunks(chunks)
+                print("I am Finish.")
+            else:
+                print("ERROR: There are no chunks.")
+        except Exception as e:
+            error(e)
     def ask_database(self, question: str):
+        self.decider.check(self.decider.decide(question))
         relevant_chunks = self.vektor.search(question, n_results=3)
         context = "\n---\n".join(relevant_chunks)
         result = self.llm.ask(question=question, type="chat", context=context)
